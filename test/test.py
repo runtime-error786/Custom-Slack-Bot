bfecc20470f640b3902e93162a2192d6f40fd1c3
runtime-error786 <mustafa782a@gmail.com>
2024-11-23 20:48:07 +0500

    practice of that api

---
+++ test/test.py
@@ -0,0 +1,277 @@
+import getpass
+import os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.docstore.document import Document
+from langchain.prompts import PromptTemplate
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import JsonOutputParser
+os.environ["TAVILY_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = "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"
+
+
+
+local_llm = "llama3.1"
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=250, chunk_overlap=0
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+db2 = Chroma.from_documents(doc_splits, embedding=embedding, persist_directory="./chroma_db/Adaptive")
+db3 = Chroma(persist_directory="./chroma_db/Adaptive", embedding_function=embedding)
+
+retriever = db3.as_retriever()
+
+
+llm = ChatOllama(model=local_llm, format="json", temperature=0)
+prompt = PromptTemplate(
+    template="""You are a teacher grading a quiz. You will be given: 
+    1/ a QUESTION
+    2/ A FACT provided by the student
+    
+    You are grading RELEVANCE RECALL:
+    A score of 1 means that ANY of the statements in the FACT are relevant to the QUESTION. 
+    A score of 0 means that NONE of the statements in the FACT are relevant to the QUESTION. 
+    1 is the highest (best) score. 0 is the lowest score you can give. 
+    
+    Explain your reasoning in a step-by-step manner. Ensure your reasoning and conclusion are correct. 
+    
+    Avoid simply stating the correct answer at the outset.
+    
+    Question: {question} \n
+    Fact: \n\n {documents} \n\n
+    
+    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question. \n
+    Provide the binary score as a JSON with a single key 'score' and no premable or explanation.
+    """,
+    input_variables=["question", "documents"],
+)
+
+retrieval_grader = prompt | llm | JsonOutputParser()
+question = "agent memory"
+docs = retriever.invoke(question)
+doc_txt = docs[1].page_content
+print(retrieval_grader.invoke({"question": question, "documents": doc_txt}))
+
+from langchain_core.output_parsers import StrOutputParser
+
+prompt = PromptTemplate(
+    template="""You are an assistant for question-answering tasks. 
+    
+    Use the following documents to answer the question. 
+    
+    If you don't know the answer, just say that you don't know. 
+    
+    Use three sentences maximum and keep the answer concise:
+    Question: {question} 
+    Documents: {documents} 
+    Answer: 
+    """,
+    input_variables=["question", "documents"],
+)
+
+llm = ChatOllama(model=local_llm, temperature=0)
+
+rag_chain = prompt | llm | StrOutputParser()
+
+
+generation = rag_chain.invoke({"documents": docs, "question": question})
+
+print(generation)
+
+
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+web_search_tool = TavilySearchResults(k=3)
+
+from typing import List
+from typing_extensions import TypedDict
+from IPython.display import Image, display
+from langchain.schema import Document
+from langgraph.graph import START, END, StateGraph
+
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
+
+    Attributes:
+        question: question
+        generation: LLM generation
+        search: whether to add search
+        documents: list of documents
+    """
+
+    question: str
+    generation: str
+    search: str
+    documents: List[str]
+    steps: List[str]
+    
+def retrieve(state):
+    """
+    Retrieve documents
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, documents, that contains retrieved documents
+    """
+    question = state["question"]
+    documents = retriever.invoke(question)
+    steps = state["steps"]
+    steps.append("retrieve_documents")
+    return {"documents": documents, "question": question, "steps": steps}
+
+def generate(state):
+    """
+    Generate answer
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, generation, that contains LLM generation
+    """
+
+    question = state["question"]
+    documents = state["documents"]
+    generation = rag_chain.invoke({"documents": documents, "question": question})
+    steps = state["steps"]
+    steps.append("generate_answer")
+    return {
+        "documents": documents,
+        "question": question,
+        "generation": generation,
+        "steps": steps,
+    }
+    
+def grade_documents(state):
+    """
+    Determines whether the retrieved documents are relevant to the question.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Updates documents key with only filtered relevant documents
+    """
+
+    question = state["question"]
+    documents = state["documents"]
+    steps = state["steps"]
+    steps.append("grade_document_retrieval")
+    filtered_docs = []
+    search = "No"
+    for d in documents:
+        score = retrieval_grader.invoke(
+            {"question": question, "documents": d.page_content}
+        )
+        grade = score["score"]
+        if grade == "yes":
+            filtered_docs.append(d)
+        else:
+            search = "Yes"
+            continue
+    return {
+        "documents": filtered_docs,
+        "question": question,
+        "search": search,
+        "steps": steps,
+    }
+
+def web_search(state):
+    """
+    Web search based on the re-phrased question.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): Updates documents key with appended web results
+    """
+
+    question = state["question"]
+    documents = state.get("documents", [])
+    steps = state["steps"]
+    steps.append("web_search")
+    web_results = web_search_tool.invoke({"query": question})
+    documents.extend(
+        [
+            Document(page_content=d["content"], metadata={"url": d["url"]})
+            for d in web_results
+        ]
+    )
+    return {"documents": documents, "question": question, "steps": steps}
+
+def decide_to_generate(state):
+    """
+    Determines whether to generate an answer, or re-generate a question.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Binary decision for next node to call
+    """
+    search = state["search"]
+    if search == "Yes":
+        return "search"
+    else:
+        return "generate"
+
+workflow = StateGraph(GraphState)
+
+workflow.add_node("retrieve", retrieve)  
+workflow.add_node("grade_documents", grade_documents) 
+workflow.add_node("generate", generate)  
+workflow.add_node("web_search", web_search) 
+
+workflow.add_edge(START, "retrieve")
+workflow.add_edge("retrieve", "grade_documents")
+workflow.add_conditional_edges(
+    "grade_documents",
+    decide_to_generate,
+    {
+        "search": "web_search",
+        "generate": "generate",
+    },
+)
+workflow.add_edge("web_search", "generate")
+workflow.add_edge("generate", END)
+
+from langgraph.checkpoint.memory import MemorySaver
+
+memory = MemorySaver()
+
+custom_graph = workflow.compile(checkpointer=memory)
+
+
+import uuid
+
+thread_id = str(uuid.uuid4())
+
+def predict_custom_agent_local_answer(example: dict):
+    config = {"configurable": {"thread_id": thread_id}}
+    state_dict = custom_graph.invoke(
+        {"question": example["input"], "steps": []}, config
+    )
+    return {"response": state_dict["generation"], "steps": state_dict["steps"]}
+
+
+example = {"input": "What are the types of agent memory?"}
+response = predict_custom_agent_local_answer(example)
+response
